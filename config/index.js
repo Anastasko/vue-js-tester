15dfd1d3d77e9a2525a5f3659685d03dd22ae360
lorry <volodja.extrim@gmail.com>
2017-11-23 01:19:17 +0200

    + files

--- config/index.js
+++ config/index.js
@@ -54,7 +54,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'vue-js-tester/static',
     assetsPublicPath: '/vue-js-tester/',
 
     /**
